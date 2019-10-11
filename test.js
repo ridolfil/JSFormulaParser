16505b3fac71a7d49e3679e960ae879aab5aae20
Luca Ridolfi <ridoluc@lucas-macbook.lan>
2019-10-11 21:50:36 +0100

    testing formulas

---
+++ test.js
@@ -0,0 +1,22 @@
+var testobj = {
+    scope:null
+    //function calculate({})
+}
+
+var add = (a,b) => (a*b);
+var mult = (a,b) => (a+b);
+
+testobj.scope = {
+        n1:3, 
+        n2:4,
+        n3:2
+    };
+
+testobj[0] = add;//(this.scope.n1,3)
+testobj[1] = mult;//(1, this.first)
+
+testobj.first(testobj.scope.n1,9)
+/*
+    @param a:Number
+*/
+Console.log(testobj.cal)
