322262941fd51e8b8254d7e9a4eddba9450a69cd
Luca Ridolfi <ridolfi.luc@gmail.com>
2019-10-13 21:23:41 +0100

    tests

--- test.js
+++ test.js
@@ -1,12 +1,23 @@
+/**
+ *  Create an object that can 
+ *  - hold a list of variables
+ *  - calculate and return a value based on the input variables
+ *  - hold a number of functions 
+ *  
+ *  the variables must be bound the the defined function so that 
+ *  
+ **/
+
 var testobj = {
     scope:null,
     0:32,
-    get value() {return this[0];}
+    value:0,
+    get value() {return value = this[0];}
     //function calculate({})
 }
 
 function mult(a,b) { return a*b;}
-var add = (a,b) => (a+b);
+function add(a,b) {return a+b};
 
 testobj.scope = {
         n1:3, 
@@ -19,16 +30,18 @@ console.log(testobj.value)
 testobj[0] = add;//(this.scope.n1,3)
 testobj[1] = mult;//(1, this.first)
 
+
 //var tmp = testobj[0](testobj.scope.n1,testobj[1]);
 /*
     @param a:Number
 */
 
 
-
+/*
 console.log(testobj[0](testobj.scope.n1,testobj[1](testobj.scope.n2,testobj.scope.n3)))
 
 testobj.scope.n2 = 13;
 
 console.log(testobj.value)
 
+*/
